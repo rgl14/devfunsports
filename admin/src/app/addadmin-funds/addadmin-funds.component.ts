45a317803b3c3b85d2a9b9cd7ed25e86426e08d5
rgl14 <52996923+rgl14@users.noreply.github.com>
2020-10-10 17:22:51 +0530

    add admin funds design added

---
+++ admin/src/app/addadmin-funds/addadmin-funds.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsermanagementService } from '../services/usermanagement.service';
+import { NotificationService } from '../shared/notification.service';
+
+@Component({
+  selector: 'app-addadmin-funds',
+  templateUrl: './addadmin-funds.component.html',
+  styleUrls: ['./addadmin-funds.component.css']
+})
+export class AddadminFundsComponent implements OnInit {
+  Adminfund: FormGroup;
+  DepositAdminfunds: any;
+  accountInfo: any;
+  totaladminbalance: number;
+  constructor(private fb: FormBuilder,private usermanagement: UsermanagementService,public notification: NotificationService,) { }
+
+  ngOnInit() {
+    this.AccountInfo();
+    this.initAdminfundForm();
+  }
+  get f() {
+    return this.Adminfund.controls;
+  }
+  get Depositfund() { return this.Adminfund.get('Depositfund') };
+  initAdminfundForm() {
+    this.Adminfund = this.fb.group({
+      Depositfund: [0, [Validators.required]],
+      remark: ['']
+    })
+    this.formControlDepositfundChanged();
+  }
+  AccountInfo() {
+    this.usermanagement.getAccountInfo().subscribe((data) => {
+      this.accountInfo = data.data;
+      this.totaladminbalance=this.accountInfo.remainingLimit;
+    });
+  }
+  formControlDepositfundChanged() {
+    this.Adminfund.get('Depositfund').valueChanges.subscribe(
+        (mode: any) => {
+          this.DepositAdminfunds=mode;
+          this.totaladminbalance=parseFloat(this.accountInfo.remainingLimit)+parseFloat(this.DepositAdminfunds)
+    });
+  }
+  Addfunds(){
+    if (!this.Adminfund.valid) {
+      return;
+    }
+    let data={
+      "amount":this.Adminfund.value.Depositfund,
+      "remarks":this.Adminfund.value.remark
+    }
+    this.usermanagement.DefineAdminFunds(data).subscribe((resp) => {
+      if (resp.status == "Success") {
+        this.notification.success(resp.result);
+        this.AccountInfo();
+        this.onCancel();
+      } else {
+        this.notification.error(resp.result);
+      }
+    })
+  }
+  onCancel() {
+    this.initAdminfundForm();
+  }
+}
